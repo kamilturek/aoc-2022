4802505cf120ffeb507af89ea021c510a234b142
Kamil Turek <kamil.turek@hotmail.com>
2022-12-22 21:36:51 +0100

    Add day11

---
+++ day11/part2.py
@@ -0,0 +1,83 @@
+import math
+import re
+import sys
+from collections import deque
+from dataclasses import dataclass
+from operator import add, mul
+from typing import Callable, Self
+
+OPERATION_PATTERN = re.compile(
+    r"Operation: new = old (?P<operator>\+|\*) (?P<arg>old|\d+)"
+)
+
+
+def get_func(operator, arg):
+    match (operator, arg):
+        case (_, "old"):
+            return lambda x: x * x
+        case ("*", arg):
+            return lambda x: x * int(arg)
+        case ("+", arg):
+            return lambda x: x + int(arg)
+
+
+@dataclass
+class Monkey:
+    items: deque[int]
+    modulo: int
+    func: Callable[[int], int]
+    target_true_index: int
+    target_false_index: int
+
+    inspections_done: int = 0
+
+    @classmethod
+    def from_text(cls, text: str) -> Self:
+        properties = [line.strip() for line in text.splitlines()]
+
+        items = re.findall(r"\d+", properties[1])
+        operator, arg = OPERATION_PATTERN.match(properties[2]).groups()
+        modulo = re.search(r"\d+", properties[3]).group()
+        target_true_index = re.search(r"\d+", properties[4]).group()
+        target_false_index = re.search(r"\d+", properties[5]).group()
+
+        return Monkey(
+            items=deque(map(int, items)),
+            func=get_func(operator, arg),
+            modulo=int(modulo),
+            target_true_index=int(target_true_index),
+            target_false_index=int(target_false_index),
+        )
+
+
+def solve(input):
+    """
+    >>> solve(open('input0.txt'))
+    2713310158
+    >>> solve(open('input1.txt'))
+    14636993466
+    """
+    input = input.read().split("\n\n")
+
+    monkeys = [Monkey.from_text(section) for section in input]
+    common_multiple = math.prod(monkey.modulo for monkey in monkeys)
+
+    for _ in range(10000):
+        for monkey in monkeys:
+            while len(monkey.items):
+                worry_level = monkey.items.popleft()
+                worry_level = monkey.func(worry_level)
+                target_index = (
+                    monkey.target_true_index
+                    if worry_level % monkey.modulo == 0
+                    else monkey.target_false_index
+                )
+                worry_level = worry_level % common_multiple
+                monkeys[target_index].items.append(worry_level)
+                monkey.inspections_done += 1
+
+    return mul(*sorted([monkey.inspections_done for monkey in monkeys])[-2:])
+
+
+if __name__ == "__main__":
+    print(solve(sys.stdin))
